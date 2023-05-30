ee6aa979687172a4bc1f8679e8ad0f80d1c39d06
exerusik <exe.rusik@gmail.com>
2023-05-30 16:38:34 +0300

    test Commit

--- app/src/test/java/hexlet/code/MainTest.java
+++ app/src/test/java/hexlet/code/MainTest.java
@@ -1,16 +1,14 @@
 package hexlet.code;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 import static hexlet.code.Main.sayHello;
-import static  org.assertj.core.api.Assertions.*;
+import static  org.assertj.core.api.Assertions.assertThat;
 
 class MainTest {
 
     @Test
     void testSayHello() {
-        String expected ="hello";
+        String expected = "hello";
         String actual = sayHello();
         assertThat(actual).isEqualTo(expected);
     }
